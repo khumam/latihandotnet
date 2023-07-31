bda0cb8531de2ea47fd80cda8abf7221134552b6
Khoerul Umam <khoerul.umam@gotoko.co.id>
2023-07-31 16:43:44 +0700

    up: database integ

--- Program.cs
+++ Program.cs
@@ -1,24 +1,24 @@
+using System.Configuration;
 using Microsoft.EntityFrameworkCore;
 using TodoAPI.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager configuration = builder.Configuration;
+IWebHostEnvironment environment = builder.Environment;
+
 builder.Services.AddControllers();
-builder.Services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDbContext<TodoContext>(opt => opt.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+if (app.Environment.IsDevelopment()) {
   app.UseSwagger();
   app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
